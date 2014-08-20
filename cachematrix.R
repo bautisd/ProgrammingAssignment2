c961757bb261395992da1a386a323739b28fab53
dave bautis <dave.bautis@gmail.com>
2014-08-20 12:32:43 -0400

    added comments, initial completed version

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## Object for storing a cachable square matrix
+## helper funcitons for get, set the matrix
+## getinv,setinv the inverse of the matrix
+## returns a list with the function definitions
 makeCacheMatrix <- function(x = matrix()) {
 
   inv <-NULL
@@ -17,18 +17,23 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse of a square matrix using the "solve" function
+## If the inverse is already know from previous execution,
+## returns the cached value of the inverse, does not recaculate the inverse
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  inv <-x$getinverse()
+  ## check to see if the inverse is known
+  inv <-x$getinv()
   if(!is.null(inv)){
     message("getting from cache")
     return(inv)
   }
+  ## inverse is not known, calculate inverse,
+  ## store new calculated invers in the cache object
   data<-x$get()
-  inv<-solve(x, ...)
-  inv$setmean(inv)
+  inv<-solve(data, ...)
+  x$setinv(inv)
   inv
 }
 
